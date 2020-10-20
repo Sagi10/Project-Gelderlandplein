1bf9229097d21852f02529057791ebf65152b208
Sagi Lalee <Sagi_lalee@hotmail.com>
2020-10-20 13:51:39 +0200

    added begin code for this fragment

--- Gelderlandplein/app/src/main/java/com/example/gelderlandplein/ui/OverviewItemsFragment.kt
+++ Gelderlandplein/app/src/main/java/com/example/gelderlandplein/ui/OverviewItemsFragment.kt
@@ -1,2 +1,26 @@
 package com.example.gelderlandplein.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class OverviewItemsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
