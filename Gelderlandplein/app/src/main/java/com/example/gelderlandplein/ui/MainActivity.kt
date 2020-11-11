1cede8c19fa347ca46a4d2a694a4065bf1168723
Kerubel Abera <kerubela1@gmail.com>
2020-11-11 15:36:20 +0100

    Toolbars met meer details toegevoegd en lege search bar

--- Gelderlandplein/app/src/main/java/com/example/gelderlandplein/ui/MainActivity.kt
+++ Gelderlandplein/app/src/main/java/com/example/gelderlandplein/ui/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.gelderlandplein.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
 import android.view.Menu
+import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.isVisible
 import androidx.navigation.NavController
@@ -23,36 +27,84 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
         setSupportActionBar(toolbar)
         supportActionBar?.title = "Test"
+        toolbar.title = ""
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         navController = findNavController(R.id.fragment)
-
+        supportActionBar?.setDisplayShowHomeEnabled(true);
         bottomNavigationView.setupWithNavController(navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_arts, menu)
-        switchMenu()
+        menuInflater.inflate(R.menu.menu_main, menu)
+        switchMenu(menu)
         return true
     }
 
-    private fun switchMenu() {
+    @SuppressLint("SetTextI18n")
+    private fun switchMenu(menu: Menu) {
         navController.addOnDestinationChangedListener { _, destination, _ ->
             if (destination.id in arrayOf(R.id.homeFragment)) {
                 toolbar.isVisible = false
             } else if (destination.id in arrayOf(R.id.SearchFragment)){
-                toolbar.title = "Search"
+                toolbar_title.text = ""
                 toolbar.isVisible = true
+                toolbar.menu.findItem(R.id.btSearch)?.isVisible = true
                 supportActionBar?.setDisplayHomeAsUpEnabled(false)
-            } else if (destination.id in arrayOf(R.id.ArtOverviewFragment)){
-                toolbar.title = "Arts"
+
+                val manager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+                val searchItem = menu.findItem(R.id.btSearch)
+                val searchView = searchItem?.actionView as SearchView
+                
+
+            } else if (destination.id in arrayOf(R.id.shopDetailFragment)){
+                toolbar_title.text = ""
                 toolbar.isVisible = true
+                toolbar.menu.findItem(R.id.btSearch)?.isVisible = false
+                supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            } else if (destination.id in arrayOf(R.id.ArtOverviewFragment)){
+                toolbar_title.text = "Art"
+                toolbar.isVisible = true
+                toolbar.menu.findItem(R.id.btSearch)?.isVisible = false
+                supportActionBar?.setDisplayHomeAsUpEnabled(false)
+            } else if(destination.id in arrayOf(R.id.ArtDetailFragment)){
+                toolbar_title.text = "Art"
+                toolbar.isVisible = true
+                toolbar.menu.findItem(R.id.btSearch)?.isVisible = false
                 supportActionBar?.setDisplayHomeAsUpEnabled(true)
             } else if (destination.id in arrayOf(R.id.EventFragment)){
-                toolbar.title = "Events"
+                toolbar_title.text = "News & Events"
                 toolbar.isVisible = true
+                toolbar.menu.findItem(R.id.btSearch)?.isVisible = false
+                supportActionBar?.setDisplayHomeAsUpEnabled(false)
+            } else if (destination.id in arrayOf(R.id.eventDetailFragment)){
+                toolbar_title.text = "News & Events"
+                toolbar.isVisible = true
+                toolbar.menu.findItem(R.id.btSearch)?.isVisible = false
+                supportActionBar?.setDisplayHomeAsUpEnabled(true)
             }
         }
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//
+//        when (item.itemId) {
+////            android.R.id.home -> {
+////                navController.navigate(androidx.navigation.R.id.action_gamesPlayedFragment2_to_gameFragment)
+////                return true
+////            }
+//
+//            else -> super.onOptionsItemSelected(item)
+//
+//        }
+//        return true
+//    }
 }
