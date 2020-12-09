601c3133851924fa5840df1e292ff1fdf52f34de
Sagi <sagi.lalee@hva.nl>
2020-12-09 10:54:44 +0100

    few changes and code clean up

--- Gelderlandplein/app/src/main/java/com/example/gelderlandplein/service/MyFirebaseMessagingService.kt
+++ Gelderlandplein/app/src/main/java/com/example/gelderlandplein/service/MyFirebaseMessagingService.kt
@@ -10,6 +10,7 @@ import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.example.gelderlandplein.R
+import com.example.gelderlandplein.helpers.UrlToBitmap
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import java.io.IOException
@@ -17,39 +18,19 @@ import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.URL
 
-
 @SuppressLint("MissingFirebaseInstanceTokenRefresh")
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
-    private val NOTIFICATION_ID = 1
-
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-
-        Log.d(TAG, "From: " + remoteMessage.from)
-
-        // Check if message contains a data payload.
-        if (remoteMessage.data.isNotEmpty()) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.data)
-
-            sendNotifiction(remoteMessage)
-        }
 
         // Check if message contains a notification payload.
         if (remoteMessage.notification != null) {
-            Log.d(TAG, "Message Notification Title: " + remoteMessage.notification!!.title)
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.notification!!.body)
-            Log.d(TAG, "Message Notification URL: " + remoteMessage.notification!!.imageUrl)
+            sendNotification(remoteMessage)
         }
 
     }
 
-    private fun sendNotifiction(remoteMessage: RemoteMessage) {
-        // Voor aangepaste gegevens kan dit worden gebruikt.
-        val data: Map<String, String> = remoteMessage.data
-        val title: String = data.getValue("title")
-        val content: String = data.getValue("content")
-        val image: String = data.getValue("image")
-
+    private fun sendNotification(remoteMessage: RemoteMessage) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val notificationChannel =
                 NotificationChannel(NOTIFICATION_CHANNEL, "Notification channel", NotificationManager.IMPORTANCE_HIGH)
@@ -60,9 +41,9 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
             val notification = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL)
                 .setSmallIcon(R.drawable.ic_logo)
-                .setContentTitle(title)
-                .setContentText(content)
-                .setLargeIcon(getBitmapFromURL(image))
+                .setContentTitle(remoteMessage.notification!!.title)
+                .setContentText(remoteMessage.notification!!.body)
+                .setLargeIcon(UrlToBitmap.getBitmapFromURL(remoteMessage.notification!!.imageUrl.toString()))
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_PROMO)
                 .build()
@@ -71,21 +52,9 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         }
     }
 
-    private fun getBitmapFromURL(src: String?): Bitmap? {
-        return try {
-            val url = URL(src)
-            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
-            connection.doInput = true
-            connection.connect()
-            val input: InputStream = connection.inputStream
-            BitmapFactory.decodeStream(input)
-        } catch (e: IOException) {
-            Log.e(TAG, "ERROR CREATING BITMAP: ${e.message}")
-            null
-        }
-    }
-
     companion object {
         const val NOTIFICATION_CHANNEL = "notification_channel"
+        const val NOTIFICATION_ID = 1
+
     }
 }
